0b7068f4494134c070ffab227eacc6490855176d
Keshab Kamal <keshabwi66@gmail.com>
2026-01-08 21:58:29 +0545

    Docker+Local+Railway Deployment  Updatedv2

--- database.py
+++ database.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASE_URL="postgresql://postgres:icqRyttJAmbbQxigspPMMhlFHNKEXFPK@postgres.railway.internal:5432/railway" #FOR RAILWAY DEPLOYMENT
+#DATABASE_URL="postgresql://postgres:Ke200207%40@localhost/pizza_delivery". FOR LOCAL USE NO DOCKER
+#DATABASE_URL=os.getenv("DATABASE_URL") 
 
 engine=create_engine(DATABASE_URL, echo=True)
 
